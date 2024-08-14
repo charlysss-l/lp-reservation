880fc2c76328adf35a2362b6a460210e9115bcad
Kehbin11 <152233268+KristineEllaRojas@users.noreply.github.com>
2024-08-14 19:26:11 +1300

    Footer
    jsx & css

--- admin/a/src/pages/Login/Login.jsx
+++ admin/a/src/pages/Login/Login.jsx
@@ -5,8 +5,8 @@ const Login = () => {
   return (
     <div className="login-container">
        <h1 className="login-title">Login</h1>
-       <input type="text" name="username" className="login-input" placeholder="username" required />
-       <input type="text" name="password" className="login-input"  placeholder="password" required />
+       <input type="text" name="username" className="login-input" placeholder="Username" required />
+       <input type="text" name="password" className="login-input"  placeholder="Password" required />
        <div className="button">
             <NavLink to={'/home'} className="login-button">Login</NavLink>
             </div>
