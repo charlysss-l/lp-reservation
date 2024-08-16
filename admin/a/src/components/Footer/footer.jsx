7701f8a244aa4d8aa48e57a66d61c08d3f2b53c0
Kristine Ella <kristineella.rojas@gmail.com>
2024-08-16 15:35:12 +1300

    footer

--- admin/a/src/components/Footer/footer.jsx
+++ admin/a/src/components/Footer/footer.jsx
@@ -5,16 +5,45 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-<<<<<<< Updated upstream
         <div className="footer-left">
-         
+          {/* Content for the left side of the footer */}
         </div>
-=======
+
+        <div className="contact-info">
+          <h2>Operating Hours</h2>
+          <h3>Launchpad Commercenter:</h3>
+          <p>Mon-Fri 8am-7pm</p>
+          <p>Sat 10am-5pm</p>
         
->>>>>>> Stashed changes
+          <h2>Contact Us</h2>
+          <p>
+            <strong>Email Inquiries:</strong> 
+            <a href="mailto:hello@launchpadcoworkingph.com">hello@launchpadcoworkingph.com</a>
+          </p>
+          <p>
+            <strong>Marketing:</strong> 
+            <a href="mailto:marketing@launchpadcoworkingph.com">marketing@launchpadcoworkingph.com</a>
+          </p>
+          <p>
+            <strong>Website:</strong> 
+            <a href="http://www.launchpadcoworkingph.com" target="_blank" rel="noopener noreferrer">
+              www.launchpadcoworkingph.com
+            </a>
+          </p>
+
+          <h2>Phone</h2>
+          <p>
+            <strong>Commercenter:</strong> 
+            (02) 8 776 3321
+            <br />
+            <strong>Globe:</strong> 
+            +63 956 395 8065
+          </p>
+        </div>
+
         <div className="footer-right">
           <p>
-            Copyright © 2024 LaunchPad CoWorking | All rights reserved | 
+            Copyright © 2024 LaunchPad CoWorking | All rights reserved | Terms and Conditions
             <a href="/terms-and-conditions"> Terms and Conditions</a>
           </p>
           <ul className="credits-list">
