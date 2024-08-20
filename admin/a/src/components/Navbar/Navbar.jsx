52438f8126191ab13da2e9656591182609ee16ed
charlysss-l <charlizemikaelanadela@gmail.com>
2024-08-20 14:38:33 +0800

    NavBar

--- admin/a/src/components/Navbar/Navbar.jsx
+++ admin/a/src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import './Navbar.css'; // Assuming you have a CSS file for styling
 const Navbar = () => {
   const [reservationDropdownOpen, setReservationDropdownOpen] = useState(false);
   const [seatDropdownOpen, setSeatDropdownOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleReservationDropdown = () => {
     setReservationDropdownOpen(!reservationDropdownOpen);
@@ -16,44 +17,48 @@ const Navbar = () => {
     setReservationDropdownOpen(false); // Close the Reservation dropdown when Seat is clicked
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="logo">Launchpad</li>
+      <div className="logo">Launchpad</div>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </button>
+      <ul className={`navbar-list ${menuOpen ? 'active' : ''}`}>
         <li className="navbar-item">
           <NavLink to={'/home'} className="pages">Home</NavLink>
         </li>
         <li 
           className="navbar-item dropdown" 
-          onMouseEnter={toggleReservationDropdown} 
-          onMouseLeave={toggleReservationDropdown}
+          onClick={toggleReservationDropdown}
         >
           <span className="dropbtn">Reservation</span>
           {reservationDropdownOpen && (
             <div className="dropdown-content">
               <NavLink to={'/admin/add-reservation'}>Add Reservation</NavLink>
               <NavLink to={'/admin/ongoing'}>Ongoing</NavLink>
-              <NavLink to={'/admin/history'}> History</NavLink>
-
+              <NavLink to={'/admin/history'}>History</NavLink>
             </div>
           )}
         </li>
-        
         <li 
           className="navbar-item dropdown1" 
-          onMouseEnter={toggleSeatDropdown} 
-          onMouseLeave={toggleSeatDropdown}
+          onClick={toggleSeatDropdown}
         >
           <span className="dropbtn1">Seat</span>
           {seatDropdownOpen && (
             <div className="dropdown-content1">
-               <NavLink to={'/admin/add-seat'}> Add Seat</NavLink>
+              <NavLink to={'/admin/add-seat'}>Add Seat</NavLink>
               <NavLink to={'/admin/seat-map'}>Seat Map</NavLink>
               <NavLink to={'/admin/seat-qr'}>Seat Details</NavLink>
             </div>
           )}
         </li>
-
         <li className="navbar-item">
           <NavLink to={'/'} className="pages">Logout</NavLink>
         </li>
