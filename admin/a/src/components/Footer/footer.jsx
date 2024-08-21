40a5d32542395c449abdaf33979671f9f3ea5823
Kristine Ella Rojas <152233268+KristineEllaRojas@users.noreply.github.com>
2024-08-21 09:36:40 +0800

    Logout

--- admin/a/src/components/Footer/footer.jsx
+++ admin/a/src/components/Footer/footer.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import './footer.css';  
+import './footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-          <p>
-            Copyright © 2024 LaunchPad CoWorking | All rights reserved | 
-            <a href="/terms-and-conditions"> Terms and Conditions</a>
-          </p>
-          <ul className="credits-list">
-            <li>Charlize Mikaela Nadela</li>
-            <li>Jojo Maninang</li>
-            <li>Kristine Ella Rojas</li>
-            <li>Kevin Joshnel Mesina</li>
-            <li>Kelly Soberano</li>
-          </ul>
-        </div>
+        <p>
+          Copyright © 2024 LaunchPad CoWorking | All rights reserved |
+          <a href="/terms-and-conditions"> Terms and Conditions</a>
+        </p>
+  
+
+        <ul className="credits-list">
+          <li>Charlize Mikaela Nadela</li>
+          <li>Jojo Maninang</li>
+          <li>Kristine Ella Rojas</li>
+          <li>Kevin Joshnel Mesina</li>
+          <li>Kelly Soberano</li>
+        </ul>
+      </div>
     </footer>
   );
 };
