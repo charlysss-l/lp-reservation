23a72f232dee0f632c568450c66b729e5e2ae945
charlysss-l <charlizemikaelanadela@gmail.com>
2024-08-21 10:08:00 +0800

    Revert "Merge branch 'main' of https://github.com/charlysss-l/lp-reservation"
    This reverts commit 9083a4271a0c92bcba1565bee99b92c2f11bb0dd, reversing
    changes made to 5fc8b8650071837b5847c53acbb18915f9db0682.

--- admin/a/src/pages/Login/Login.jsx
+++ admin/a/src/pages/Login/Login.jsx
@@ -1,19 +1,17 @@
-import { NavLink } from 'react-router-dom';
-import './Login.css';
 
+import {NavLink} from 'react-router-dom'
+import './Login.css'
 const Login = () => {
   return (
-    <div className="container">
-      <div className="login-container">
-        <h1 className="login-title">Login</h1>
-        <input type="text" name="username" className="login-input" placeholder="Username" required />
-        <input type="password" name="password" className="login-input" placeholder="Password" required />
-        <div className="button">
-          <NavLink to='/home' className="login-button">Login</NavLink>
-        </div>
-      </div>
+    <div className="login-container">
+       <h1 className="login-title">Login</h1>
+       <input type="text" name="username" className="login-input" placeholder="Username" required />
+       <input type="text" name="password" className="login-input"  placeholder="Password" required />
+       <div className="button">
+            <NavLink to={'/home'} className="login-button">Login</NavLink>
+            </div>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
