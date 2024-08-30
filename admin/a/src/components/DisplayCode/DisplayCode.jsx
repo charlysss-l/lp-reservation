f1c0364e0ccb885c4fc3606be89fa4ed1163d7c1
rawr <110759203+UrWebDev@users.noreply.github.com>
2024-08-30 11:20:05 +0800

    Revert "Merge branch 'main' of https://github.com/charlysss-l/lp-reservation"
    This reverts commit 7c37f56c757e55097298d6331f10b6d0b81dd492, reversing
    changes made to f8cd5f6968daf6e8c7d4581420eaf54f08c589dd.

--- admin/a/src/components/DisplayCode/DisplayCode.jsx
+++ admin/a/src/components/DisplayCode/DisplayCode.jsx
@@ -6,24 +6,12 @@ const DisplayCode = () => {
     const location = useLocation();
     const { code } = location.state || { code: '' };
 
-    const imageUrl = `http://localhost:3000/Images/${code}`; // Construct the URL based on the code
-
-
     return (
         <div className="display-container">
         <h1 className="display-title">Reservation Successful!</h1>
         
         <div className="button-display-container">
-        <p>Your Reservation Code: 
-
-          <br/>
-          {code ? (
-                    <img src={imageUrl} alt="Reservation Code" className="display-image" />
-                ) : (
-                    'No Image Available'
-                )}</p>
-
-        
+        <p>Your Reservation Code: {code}</p>
         </div>
       </div>
                
@@ -32,5 +20,3 @@ const DisplayCode = () => {
 };
 
 export default DisplayCode;
-
-
