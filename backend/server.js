446d9700f293e99c1c5b350c55da9097ce9db9b2
charlysss-l <charlizemikaelanadela@gmail.com>
2024-09-04 10:16:24 +0800

    server.js and DisplayCode.jsx

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,8 @@ app.use(cors({
     credentials: true
 }));
 
-const port = process.env.PORT || 3000; // Use environment variable for port
+const port = process.env.PORT || 3000; 
 
-app.use(cors());
 
 app.use(express.json());
 app.use('/Images', express.static(path.join(__dirname, 'public/images')));
