1c96c42917b4a0846f3e2e68ebe0d8016f725928
charlysss-l <charlizemikaelanadela@gmail.com>
2024-09-05 22:56:32 +0800

    Push final home css

--- admin/a/src/pages/Home/Home.jsx
+++ admin/a/src/pages/Home/Home.jsx
@@ -6,8 +6,11 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home-container">
-      <h1 className="home-title">Welcome to  Reservation System!</h1>
-      
+      <h1 className="home-title">Launchpad Coworking</h1>
+      <p className="home-para">
+        At Launchpad Coworking, we redefine the way you work. Discover exceptional spaces designed to elevate your productivity 
+        and creativity.
+      </p>
       <div className="button-container">
         <NavLink to={'/admin/seat-map'} className="home-availability">
           Check Availability
