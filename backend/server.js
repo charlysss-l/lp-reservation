799907c8e34884c247ac770cb81d95d9eaa23263
Kel Soberano <105686280+charlysss-l@users.noreply.github.com>
2024-09-05 22:07:42 +0800

    AuthLoginUpdate

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,11 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB', err));
 
