e00a128dee8d391fabf0072e8690e44137d04629
Kel Soberano <kellsoberano@gmail.com>
2024-09-11 12:57:54 +0800

    UPDATEgoldSEAT

--- admin/a/src/components/Modal/Modal.jsx
+++ admin/a/src/components/Modal/Modal.jsx
@@ -5,8 +5,8 @@ const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay">
-      <div className="modal-content">
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={e => e.stopPropagation()}>
         <button className="modal-close" onClick={onClose}>X</button>
         {children}
       </div>
