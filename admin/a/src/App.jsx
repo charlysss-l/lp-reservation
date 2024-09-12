11aa0695ad1185e8cbe9084cf6bdfee993c35594
Kel Soberano <kellsoberano@gmail.com>
2024-09-13 04:51:50 +0800

    refreshPage

--- admin/a/src/App.jsx
+++ admin/a/src/App.jsx
@@ -1,11 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter , Router} from 'react-router-dom';
 import Admin from './pages/Admin';
 import 'core-js/stable';
 import './App.css';
+import router from '../../../backend/src/routes/login';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/lp-reservation"
+>
+    
       <Admin />
     </BrowserRouter>
   );
