76109f68dd2c9dd10080fc4667b5f00cb71b9aa5
Kel Soberano <kellsoberano@gmail.com>
2024-09-13 05:04:16 +0800

    Revert "refreshPage"
    This reverts commit 11aa0695ad1185e8cbe9084cf6bdfee993c35594.

--- admin/a/src/App.jsx
+++ admin/a/src/App.jsx
@@ -1,14 +1,11 @@
-import { BrowserRouter , Router} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Admin from './pages/Admin';
 import 'core-js/stable';
 import './App.css';
-import router from '../../../backend/src/routes/login';
 
 const App = () => {
   return (
-    <BrowserRouter basename="/lp-reservation"
->
-    
+    <BrowserRouter>
       <Admin />
     </BrowserRouter>
   );
